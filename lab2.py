377263de494c023cbe1afeed405285a98d8826c9
genius-2 <72036493+genius-2@users.noreply.github.com>
2020-09-28 20:10:47 +0300

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -5,9 +5,9 @@ def p(a,b):
     p=2*(a+b)
     return p
 a,b=map(int, input().split())
-print("square is")
+print("area is")
 print(s(a,b))
-print("perimetr is")
+print("perimeter is")
 print(p(a,b))
 print("type 'end' to exit ")
 e=str(input())
